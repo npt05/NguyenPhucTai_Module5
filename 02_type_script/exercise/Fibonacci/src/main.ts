7433ba9878686f8f5634c77a596e847c54ad1dad
TechCare <you@example.com>
2021-09-30 10:42:20 +0700

    commit

--- 02_type_script/exercise/Fibonacci/src/main.ts
+++ 02_type_script/exercise/Fibonacci/src/main.ts
@@ -9,11 +9,16 @@ function fibonacci(i: number): number {
     }
     return fibonacci(i-1) + fibonacci(i-2);
 }
+
 let sum = 0;
 let n= 20;
+
 console.log("Dãy số fibonacci là : ");
 for (let i = 0; i < n; i++) {
     console.log(fibonacci(i));
     sum += fibonacci(i);
 }
 console.log("Tổng dãy số fibonaci là : " +sum);
+
+
+
