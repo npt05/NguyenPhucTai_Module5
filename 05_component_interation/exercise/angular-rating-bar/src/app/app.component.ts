a47be4ae7ca2b7e046da7734cf5e5f8a68625df8
TechCare <you@example.com>
2021-10-01 15:46:20 +0700

    commit

--- 05_component_interation/exercise/angular-rating-bar/src/app/app.component.ts
+++ 05_component_interation/exercise/angular-rating-bar/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {IRatingUnit} from './i-rating-unit';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-rating-bar';
+  iRatingUnit: IRatingUnit = {
+    value: 3,
+    active: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  };
+  tich(active: number) {
+    this.iRatingUnit.value = active ;
+  }
 }
